f01b0da8e9a5498fd398de5bcdf76ce77d854606
buddyeventcoder <eml2syam@gmail.com>
2016-12-16 21:47:00 -0500

    initial commit

---
+++ src/main/java/com/buddyevents/BuddyeventsBatchApplication.java
@@ -0,0 +1,12 @@
+package com.buddyevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyeventsBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddyeventsBatchApplication.class, args);
+	}
+}
