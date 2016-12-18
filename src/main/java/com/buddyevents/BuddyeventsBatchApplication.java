a0e1cf882a0ec4137fb0f249980961d03d82d0af
buddyeventcoder <eml2syam@gmail.com>
2016-12-18 09:16:56 -0500

    pointing to config server for properties

--- src/main/java/com/buddyevents/BuddyeventsBatchApplication.java
+++ src/main/java/com/buddyevents/BuddyeventsBatchApplication.java
@@ -2,11 +2,28 @@ package com.buddyevents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Publisher;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.support.GenericMessage;
 
+@EnableBinding(Source.class)
 @SpringBootApplication
 public class BuddyeventsBatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuddyeventsBatchApplication.class, args);
 	}
+	
+	 @Bean
+	 @InboundChannelAdapter(value = Source.OUTPUT)
+	 public MessageSource<String> timerMessageSource() {
+	        return () -> new GenericMessage<>("Syam");
+	    }
+
+	
+	
 }
